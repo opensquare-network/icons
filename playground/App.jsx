d86a78ac42f33f49dd0916deaff7e8d1b74bf699
2nthony <ant.hu@outlook.com>
2024-02-02 18:24:49 +0800

    chore: update playground [skip ci]

--- playground/App.jsx
+++ playground/App.jsx
@@ -1,9 +1,18 @@
-import { SystemLoading } from "../subsquare";
+import {
+  ChainInterlayDark,
+  ChainInterlayLight,
+  LinkSubsocial,
+} from "../opensquare";
+import { SystemEdit, SystemLoading } from "../subsquare";
 
 export default function App() {
   return (
-    <div>
+    <div style={{ color: "red" }}>
       <SystemLoading />
+      <SystemEdit />
+      <ChainInterlayLight />
+      <ChainInterlayDark />
+      <LinkSubsocial />
     </div>
   );
 }
