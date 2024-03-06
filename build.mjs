280a042f62ec34716b43122a942ace628bd67968
2nthony <ant.hu@outlook.com>
2024-03-06 12:43:54 +0800

    fix: dts export missed (#5)
    the first file

--- build.mjs
+++ build.mjs
@@ -1,6 +1,8 @@
 import { spawnSync } from "node:child_process";
-import { resolve } from "node:path";
+import { resolve, basename } from "node:path";
 import { jsxDir, srcEntries, svgDir } from "./shared.mjs";
+import fg from "fast-glob";
+import { writeFileSync } from "node:fs";
 
 gen();
 
@@ -22,6 +24,7 @@ function gen() {
     ]);
 
     if (iconsRes.status !== 0) {
+      console.error(iconsRes.stderr.toString());
       process.exit(iconsRes.status);
     }
 
@@ -42,7 +45,28 @@ function gen() {
       "--clean",
     ]);
     if (compileRes.status !== 0) {
+      console.error(compileRes.stderr.toString());
       process.exit(compileRes.status);
     }
+
+    // HACK: force override `{set}/index.d.ts`
+    // did not know why the first file ignored
+    console.info(`[${set}]`, "overriding `index.d.ts`");
+
+    const dtsFiles = fg.sync("**/*.d.ts", {
+      cwd: set,
+      ignore: ["index.d.ts"],
+    });
+    const dtsString =
+      dtsFiles
+        .map((filename) => {
+          const dtsExtname = ".d.ts";
+          const name = basename(filename, dtsExtname);
+
+          return `export { default as ${name} } from './${name}.js';`;
+        })
+        .join("\n") + `\nimport 'react'`;
+
+    writeFileSync(resolve(set, "index.d.ts"), dtsString, "utf8");
   }
 }
